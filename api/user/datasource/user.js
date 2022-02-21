35f44ba121538cf919c4191a74f55ec70a93923f
Luciane Santos <lucianesantcs@gmail.com>
2022-02-21 13:42:39 -0300

    feat: unindo endpoits

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -7,11 +7,22 @@ class UsersAPI extends RESTDataSource {
   }
 
   async getUsers() {
-    return this.get("/users");
+    // return this.get("/users");
+    const users = await this.get("/users");
+    return users.map(async user => ({
+      id: user.id,
+      nome: user.nome,
+      email: user.email,
+      ativo: user.ativo,
+      role: await this.get(`/roles/${user.role}`) // pegar o objeto correspondente de acordo com a query
+    }))
   }
 
   async getUserById(id) {
-    return this.get(`/users/${id}`);
+    // return this.get(`/users/${id}`);
+    const user = await this.get(`/users/${id}`);
+    user.role = await this.get(`/roles/${user.role}`);
+    return user;
   }
 }
 

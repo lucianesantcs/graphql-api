9f1c626832f49540fe4161bd27f673dea3b81502
Luciane Santos <lucianesantcs@gmail.com>
2022-02-23 15:50:36 -0300

    feat: criado interface e modificado schema e chamadas

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -4,6 +4,10 @@ class UsersAPI extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = "http://localhost:3000";
+    this.respostaCustom = {
+      code: 200,
+      mensagem: "operação efetuada com sucesso"
+    }
   }
 
   async getUsers() {
@@ -40,14 +44,17 @@ class UsersAPI extends RESTDataSource {
     const role = await this.get(`/roles?type=${novosDados.user.role}`);
     await this.put(`/users/${novosDados.id}`, { ...novosDados.user, role: role[0].id });
     return ({
+      ...this.respostaCustom,
+      user: {
       ...novosDados.user,
       role: role[0]
+      }
     })
   }
 
   async deletaUser(id) {
     await this.delete(`/users/${id}`);
-    return id
+    return this.respostaCustom
   }
 }
 

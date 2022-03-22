6ebec19659353507c82610b0034510a5496870e6
Luciane Santos <lucianesantcs@gmail.com>
2022-03-22 13:29:57 -0300

    feat: adicionado metodo para criaçao de um registro

--- api/class/datasource/class.js
+++ api/class/datasource/class.js
@@ -18,6 +18,16 @@ class ClassAPI extends SQLDataSource {
       .where({ id: Number(id) })
     return getClass[0]
   }
+
+  async addClass(newClass) {
+    const newClassId = await this.db
+      .insert(newClass)
+      .returning('id')
+      .into('turmas')
+    
+    const insertedClass = await this.getClass(newClassId[0]);
+    return ({ ...insertedClass })
+  }
 }
 
 module.exports = ClassAPI;
--- api/class/resolvers/classResolvers.js
+++ api/class/resolvers/classResolvers.js
@@ -1,19 +1,21 @@
-// const turmas = [
-//   {
-//     id: 1,
-//     descricao: "básico"
-//   },
-//   {
-//     id: 2,
-//     descricao: "intermediário"
-//   }
-// ]
-
+const { GraphQLScalarType } = require("graphql")
 
 const classResolvers = {
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: "string de data e hora no formato ISO-8601",
+    serialize: (value) => value.toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value)
+  }),
+
   Query: {
     turmas: (root_, __, { dataSources }) => dataSources.classAPI.getClasses(),
     turma: (root_, { id }, { dataSources }) => dataSources.classAPI.getClass(id)
+  },
+
+  Mutation: {
+    incluiTurma: (_, { turma }, { dataSources }) => dataSources.classAPI.addClass(turma)
   }
 }
 
